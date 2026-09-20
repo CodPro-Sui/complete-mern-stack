cbd595ce6187e2472344e31f20ccac6275397c7c
CodPro Sui <codprosui@gmail.com>
2026-09-20 14:38:38 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -138,7 +138,7 @@ const App = () => {
                         <td>
                           <div className="profile">
                             <img
-                              src={`http://localhost:3030/uploads/${data.avatar}`}
+                              src={`https://codpro-technology-api.onrender.com/uploads/${data.avatar}`}
                               alt="Team_Member"
                             />
                           </div>
