f025103320a77625d63b363cb612143488fb1861
Naushad Ansari <108164715+CodPro-Sui@users.noreply.github.com>
2026-09-21 07:30:28 +0530

    Configure CORS to allow specific client URL

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ dataBase();
 app.set("trust proxy",1);
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
-app.use(cors());
+app.use(cors({origin:process.env.CLIENT_URL}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
